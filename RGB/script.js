a33095c436d74c1c4874bef69395ae434426c5fd
nicolasdsa <69026498+nicolasdsa@users.noreply.github.com>
2021-03-16 23:36:29 -0300

    Add files via upload

--- RGB/script.js
+++ RGB/script.js
@@ -1,49 +1,25 @@
-var aleatorio = 0;
-var min = 0;
-var max = 255;
-var quadrado = document.getElementById('quadrado');
-quadrado.style.backgroundColor = 'black';
+window.addEventListener("load", start);
 
-var Random = document.getElementById('random');
-var Verm = document.getElementById('Verm');
-var Verd = document.getElementById('Verd');
-var Azu = document.getElementById('Azu');
+function start() {
 
-Verm.addEventListener('input', CorRgb);
-Verd.addEventListener('input', CorRgb);
-Azu.addEventListener('input', CorRgb);
-Random.addEventListener('click', CorAleatoria);
+    var range = document.querySelector("#range");
+    range.addEventListener("input", mudou);
 
-var VermText = document.getElementById('VermText');
-var VerdText = document.getElementById('VerdText');
-var AzuText = document.getElementById('AzuText');
+    var text = document.querySelector("#text");
+    text.addEventListener("input", texto);
 
-function CorAleatoria() {
-  min = Math.ceil(0);
-  max = Math.floor(255);
-  atualVerm = Math.floor(Math.random() * (255 - 0));
-  atualVerd = Math.floor(Math.random() * (255 - 0));
-  atualAzu = Math.floor(Math.random() * (255 - 0));
-
-  VerdText.value = atualVerd;
-  VermText.value = atualVerm;
-  AzuText.value = atualAzu;
-
-  Verm.value = atualVerm;
-  Verd.value = atualVerd;
-  Azu.value = atualAzu;
-
-  return (quadrado.style.backgroundColor = `rgb(${atualVerm},${atualVerd},${atualAzu})`);
+    var fundo = document.querySelector("#fundo");
+    fundo
 }
 
-function CorRgb() {
-  var atualVerm = document.getElementById('Verm').value;
-  var atualVerd = document.getElementById('Verd').value;
-  var atualAzu = document.getElementById('Azu').value;
-
-  VermText.value = Verm.value;
-  VerdText.value = Verd.value;
-  AzuText.value = Azu.value;
-
-  quadrado.style.backgroundColor = `rgb(${atualVerm},${atualVerd},${atualAzu})`;
+function mudou() {
+    text.value = range.value
+    
+     return range.value;
 }
+
+function texto(){
+    text.value = range.value
+     return console.log(text.value);
+}
+
