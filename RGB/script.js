0909a945367548af55f41ff1c80a6f30323197bd
nicolasdsa <69026498+nicolasdsa@users.noreply.github.com>
2021-03-17 22:40:48 -0300

    Add files via upload

--- RGB/script.js
+++ RGB/script.js
@@ -1,25 +1,37 @@
 window.addEventListener("load", start);
 
 function start() {
-
-    var range = document.querySelector("#range");
-    range.addEventListener("input", mudou);
-
-    var text = document.querySelector("#text");
-    text.addEventListener("input", texto);
-
     var fundo = document.querySelector("#fundo");
-    fundo
+    fundo.addEventListener("input", mudou);
+
+    var vermelho = document.querySelector("#vermelho");
+    vermelho.addEventListener("input", mudou);
+
+    var vermelhot = document.querySelector("#vermelhot");
+    vermelhot.addEventListener("input", mudou);
+
+    var verde = document.querySelector("#verde");
+    verde.addEventListener("input", mudou);
+
+    var verdet = document.querySelector("#verdet");
+    verdet.addEventListener("input", mudou);
+
+    var azul = document.querySelector("#azul");
+    azul.addEventListener("input", mudou);
+
+    var azult = document.querySelector("#azult");
+    azult.addEventListener("input", mudou);  
 }
 
 function mudou() {
-    text.value = range.value
+
+    vermelhot.value = vermelho.value;
+    verdet.value = verde.value;
+    azult.value = azul.value;
+    var cor = fundo.style.backgroundColor = "rgb(" + vermelho.value + "," + verde.value + "," + azul.value + ")";
     
-     return range.value;
+    return vermelhot.value, verdet.value, azult.value, cor ;
 }
 
-function texto(){
-    text.value = range.value
-     return console.log(text.value);
-}
 
+  
