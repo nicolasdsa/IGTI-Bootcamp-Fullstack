bf5dfa82fe9920a8b949145aaef8e9f37b51d340
nicolasdsa <69026498+nicolasdsa@users.noreply.github.com>
2021-03-22 23:41:07 -0300

    Add files via upload

--- CRUD/script.js
+++ CRUD/script.js
@@ -1,12 +1,11 @@
 /* Script */
-console.log("teste")
 window.addEventListener("load",start);
 
 var globalNames = ["Um","Dois","Três","Quatro"];
 var inputName = null;
 
 function start() {
-    inputName = document.querySelector("#inputName");
+    var inputName = document.querySelector("#inputName");
 
 
     preventFormSubmit();
@@ -28,29 +27,49 @@ function preventFormSubmit(){
 function activateInput(){
     function insertName(newName){
         globalNames.push(newName)
+        console.log(globalNames)
     }
+
 
     function handleTyping(event){
         if (event.key === "Enter"){
             insertName(event.target.value)
+            console.log("teste")
         }
     }
+
+ 
+    
     inputName.focus();
     inputName.addEventListener("keyup",handleTyping);
+    
 }
 
 function render(){
-    console.log("teste")
     var divNames = document.querySelector("#names");
 
     var ul = document.createElement("ul");
+    var li1 = document.createElement("li");
+    var li2 = document.createElement("li");
+    li1.textContent = "Primeiro";
+    li2.textContent = "Segundo";
+    ul.appendChild(li1);
+    ul.appendChild(li2);
 
-    for (var i = 0; i < globalNames.length; i++){
+    divNames.appendChild(ul);
+
+    /*for (var i = 0; i < globalNames.length; i++){
         var currentName = globalNames[i];
-        console.log(i)
         var li = document.createElement("li");
         li.textContent = currentName;
         ul.appendChild(li);
     }
     divNames.appendChild(ul);
+    clearInput();*/
+
+}
+
+function clearInput(){
+    inputName.value = "";
+    inputName.focus();
 }
