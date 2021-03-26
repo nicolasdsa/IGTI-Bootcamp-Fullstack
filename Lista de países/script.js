f7a172387d9cf3b4c8a6cc6b3f8918b5b7f6e85f
nicolasdsa <69026498+nicolasdsa@users.noreply.github.com>
2021-03-25 23:58:02 -0300

    Add files via upload

--- Lista de países/script.js
+++ Lista de países/script.js
@@ -1,1 +1,19 @@
 /* script */
+
+window.addEventListener("load", () => {
+   fetchCountries();
+})
+
+async function fetchCountries(){
+     let Countries = await fetch("https://restcountries.eu/rest/v2/all")
+     let CountriesJson = await Countries.json();
+
+     let CountriesMap = await CountriesJson.map(country => {
+        let {
+            name,
+            population
+        } = CountriesJson
+
+        return console.log(CountriesJson)
+     })
+}
